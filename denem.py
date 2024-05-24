629152112c8ce0eddb8d805dfc6dba3fa57afe77
Dirshaye Y. Faltamo <212802094@ogr.cbu.edu.tr>
2024-05-24 18:04:31 +0300

    Update denem.py

--- denem.py
+++ denem.py
@@ -1,11 +1,13 @@
 import datetime
 
-
+# Bugünün tarihini al
 trh1 = datetime.date.today()
-trh2= trh1 + datetime.timedelta(days=1)
-print(trh1,trh2)
+# Yarın için tarih hesapla
+trh2 = trh1 + datetime.timedelta(days=1)
+print(trh1, trh2)
 
-fark = trh1-trh2
+# Tarih farkını hesapla ve mutlak değerini al
+fark = trh1 - trh2
 fark = fark.days
 fark = abs(fark)
 print(fark)
